a49a69e09ef38c6cf9eda6e569fab43104d2bfb8
irgi <irgiaditpratama@gmail.com>
2025-09-27 21:45:23 +0700

    4.4.1

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  base: "/To-do-list-App", 
   resolve: {
     alias: {
       // Gunakan cara ini, ini adalah standar modern untuk Vite
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+    
   }
 })
+
